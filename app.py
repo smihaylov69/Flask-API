903a579e33198cef6071a868e9f70f4489776d01
smihaylov69 <smihaylov208@gmail.com>
2024-06-12 12:41:50 +0300

    Createing templates

--- app.py
+++ app.py
@@ -1,14 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2024'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2024'
+    }
+]
+
+
 @app.route("/")
+@app.route("/home")
 def hello():
-    return "<h1>Hello World</h1>"
+    return render_template("home.html")
 
 @app.route("/about")
 def about():
-    return "<h1>About World</h1>"
+    return render_template("about.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
