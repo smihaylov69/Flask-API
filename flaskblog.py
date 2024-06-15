739013f66038598e737e4d813d7280e14e41026a
smihaylov69 <smihaylov208@gmail.com>
2024-06-15 12:11:06 +0300

    Creatin login and register routes

--- flaskblog.py
+++ flaskblog.py
@@ -25,11 +25,25 @@ posts = [
 @app.route("/")
 @app.route("/home")
 def hello():
-    return render_template("home.html", posts = posts)
+    return render_template('home.html', posts = posts)
+
 
 @app.route("/about")
 def about():
-    return render_template("about.html", title = "About Page")
+    return render_template('about.html', title = 'About')
+
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title = 'Register', form = form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title = 'Login', form = form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
