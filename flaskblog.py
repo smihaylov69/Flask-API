a65c37aac30a450565bd8c4c1393e06b4cfdeabf
smihaylov69 <smihaylov208@gmail.com>
2024-06-16 11:55:31 +0300

    Creating Data BAse

--- flaskblog.py
+++ flaskblog.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template,url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '479a33318c149edff83e99705e00d3f1'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
 
 
 posts = [
