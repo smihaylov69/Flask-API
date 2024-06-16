aa1c5b6b172f57e8753f8b7a26237e25e0a35f97
smihaylov69 <smihaylov208@gmail.com>
2024-06-16 12:13:26 +0300

    Creating a repr for user model

--- flaskblog.py
+++ flaskblog.py
@@ -14,6 +14,9 @@ class User(db.Model):
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
 
+def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
 posts = [
     {
         'author': 'Corey Schafer',
