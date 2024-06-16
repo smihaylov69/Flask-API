adeff89688f95b5c8c4d1d8b3b9629d1a6f08cfe
smihaylov69 <smihaylov208@gmail.com>
2024-06-16 21:20:43 +0300

    Fixing syntax error

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -5,3 +5,5 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '479a33318c149edff83e99705e00d3f1'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+
+from flaskblog import routes
