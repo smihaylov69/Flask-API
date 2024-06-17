cee9d4026032ee66db0cc14b1288ac43dfe9f320
smihaylov69 <smihaylov208@gmail.com>
2024-06-17 12:06:16 +0300

    venv

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '479a33318c149edff83e99705e00d3f1'
