a9002a706a1583e8542e041504d36c9fdc5e6544
smihaylov69 <smihaylov208@gmail.com>
2024-06-17 22:52:16 +0300

    Creating a user loader

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -2,6 +2,11 @@ from datetime import datetime
 from flaskblog import db, login_manager
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
