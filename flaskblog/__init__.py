d01cb28162053d797fac2b20b4f05ae4556848fe
smihaylov69 <smihaylov208@gmail.com>
2024-06-17 21:33:50 +0300

    Importin Lopginmanager in init.py

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '479a33318c149edff83e99705e00d3f1'
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from flaskblog import routes
