643ec20674df6ba5bd1e9eafa848c9e3dac3e48f
smihaylov69 <smihaylov208@gmail.com>
2024-06-18 22:08:55 +0300

    Now you can update picture

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,7 +1,10 @@
+import os
+import secrets
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
-from flaskblog.models import User
+from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
 
@@ -67,21 +70,3 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('home'))
-
-
-@app.route("/account", methods=['GET', 'POST'])
-@login_required
-def account():
-    form = UpdateAccountForm()
-    if form.validate_on_submit():
-        current_user.username = form.username.data
-        current_user.email = form.email.data
-        db.session.commit()
-        flash('Your account has been updated!', 'success')
-        return redirect(url_for('account'))
-    elif request.method == 'GET':
-        form.username.data = current_user.username
-        form.email.data = current_user.email
-    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
-    return render_template('account.html', title='Account', image_file=image_file, form=form)
-
