b28d4332e6e39d912ea187ef7bc2ea2c745087e8
smihaylov69 <smihaylov208@gmail.com>
2024-06-19 13:33:30 +0300

    tt

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, request, Blueprint
-from flaskblog.model import Post
+from flaskblog.models import Post
 
 main = Blueprint('main', __name__)
 
--- flaskblog/posts/routes.py
+++ flaskblog/posts/routes.py
@@ -2,7 +2,7 @@ from flask import (render_template, url_for, flash,
                    redirect, request, abort, Blueprint)
 from flask_login import current_user, login_required
 from flaskblog import db
-from flaskblog.model import Post
+from flaskblog.models import Post
 from flaskblog.posts.forms import PostForm
 
 posts = Blueprint('posts', __name__)
